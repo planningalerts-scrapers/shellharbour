c4b7e1c224f671351962162918864ffc4b9f8753
Eric Tam <erictam01@gmail.com>
2015-12-06 23:31:28 +1100

    Initial write

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-This is a scraper that runs on [Morph](https://morph.io). To get started [see the documentation](https://morph.io/documentation)
+# Shellharbour City Council Scraper
+
+Shellharbour Council involves the followings
+* Server - Apache/2.2.4 (Win32) mod_ssl/2.2.3 OpenSSL/0.9.8e PHP/5.2.0
+* Cookie tracking - No
+* Pagnation - No
+* Javascript - No
+* Clearly defined data within a row - Acceptable
+* Some record have missing 'Address'
+
+## There is no submitted data available, therefore, using Exhibition end date minus 14 days equals to submitted date is used.
+
+Enjoy
--- scraper.php
+++ scraper.php
@@ -1,27 +1,66 @@
-<?
-// This is a template for a PHP scraper on morph.io (https://morph.io)
-// including some code snippets below that you should find helpful
+<?php
+# Shellharbour City Council scraper
+require 'scraperwiki.php';
+require 'simple_html_dom.php';
+date_default_timezone_set('Australia/Sydney');
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
 
-// You don't have to do things with the ScraperWiki library.
-// You can use whatever libraries you want: https://morph.io/documentation/php
-// All that matters is that your final data is written to an SQLite database
-// called "data.sqlite" in the current working directory which has at least a table
-// called "data".
+###
+### Main code start here
+###
+$url_base = "http://www.oasis.shellharbour.nsw.gov.au";
+$da_page  = $url_base . "/index.pl?page=3442";
+
+# Get the data that I want within the page
+$dom = file_get_html($da_page);
+
+foreach($dom->find("tr[bgcolor=#DEE1E7]") as $record ) {
+
+    $application = array('council_reference' => '', 'address' => '', 'description' => '', 'info_url' => '', 
+                         'comment_url' => '', 'date_scraped' => '', 'date_received' => '');
+
+    foreach($record->find('tr') as $gem) {
+        if ((!is_null($gem->find("td", 0))) && (!is_null($gem->find("td", 1)))) {
+            $key = trim($gem->find("td", 0)->plaintext);
+            $value = preg_replace('/\s+/', ' ', trim($gem->find("td", 1)->plaintext));
+        }
+
+        switch ($key) {
+            case 'DA Number:' :
+                $value = str_replace("Lodge a Submission", "", $value);
+                $application['council_reference'] = $value;
+                $application['comment_url']       = $url_base . $gem->find("a", 0)->href;
+                break;            
+            case 'Address:' :
+                if (!empty($value)) {
+                    $application['address'] = $value . ", Australia";
+                } else {
+                    $application['address'] = " ";
+                }
+                break;
+            case 'Description:' :
+                $application['description'] = $value;
+                break;
+            case 'Notification Expires:' :
+                $date_received = substr($value, 4);
+                $application['on_notice_to']  = date('Y-m-d', strtotime($date_received));
+                $application['date_received'] = date('Y-m-d', strtotime($date_received) - (14*24*60*60));   # Submit date = Notification - 14 days (Guessing??)               
+                break;                
+        }
+    }
+    $application['info_url'] = $da_page;
+    $application['date_scraped'] = date('Y-m-d');
+
+    # Check if record exist, if not, INSERT, else do nothing
+    $existingRecords = scraperwiki::select("* from data where `council_reference`='" . $application['council_reference'] . "'");
+    if ((count($existingRecords) == 0) && ($application['council_reference'] !== 'Not on file')) {
+        print ("Saving record " . $application['council_reference'] . "\n");
+         print_r ($application);
+        #scraperwiki::save(array('council_reference'), $application);
+    } else {
+        print ("Skipping already saved record or ignore corrupted data - " . $application['council_reference'] . "\n");
+    }
+
+}
+
 ?>
