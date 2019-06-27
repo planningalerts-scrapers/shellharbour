601910dd184f6f01b1c7d6a935fdd6c596a1e911
Matthew Landauer <mlandauer@gmail.com>
2019-06-27 16:41:04 +1000

    Upgrade to latest gem

--- scraper.rb
+++ scraper.rb
@@ -1,9 +1,3 @@
 require "technology_one_scraper"
 
-TechnologyOneScraper.scrape_and_save_period(
-  url: "https://eservices.shellharbour.nsw.gov.au/T1PRProd/WebApps/eProperty",
-  period: "L28",
-  webguest: "SCC.WEBGUEST",
-  # Looks like the site has an incomplete certificate chain
-  disable_ssl_certificate_check: true
-)
+TechnologyOneScraper.scrape_and_save(:shellharbour)
